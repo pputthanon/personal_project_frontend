9d9b988a5c799496a07cccaa3a734d8611be5265
pputthanon <p.putthanon@gmail.com>
2023-10-12 17:02:51 +0700

    redo header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/use-auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const { authUser } = useAuth();
+  const navigate = useNavigate();
   return (
     <header className=" grid grid-cols-2 py-4 bg-orange-300 shadow-lg top-0 z-30 w-screen ">
       <div>
         <Link to="/homepage">Onmyway</Link>
       </div>
       <div className="flex justify-end gap-4 ">
-        <Link to="/cart">
-          <span>Cart</span>
-        </Link>
-        <Link to="/login">
-          <span>Login</span>
-        </Link>
-        <Link to="/register">
-          <span>Register</span>
-        </Link>
-        <span>Dropdown</span>
+        {authUser ? (
+          <>
+            <Link to="/cart">
+              <span>Cart</span>
+            </Link>
+            <span>Dropdown</span>
+          </>
+        ) : (
+          <>
+            <Link to="/register">
+              <span>Register</span>
+            </Link>{" "}
+            <Link to="/login">
+              <span>Login</span>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,19 +6,5 @@ export default function LoginPage() {
     <div>
       <LoginForm />
     </div>
-
-    // <div>
-    //   <header className=" grid grid-cols-2 py-4 bg-orange-300 shadow-lg top-0 z-30 w-screen">
-    //     <div>Onmyway</div>
-    //     <div className="flex justify-end gap-4">
-    //       <Link to="/register">
-    //         <span>Register</span>
-    //       </Link>
-    //     </div>
-    //   </header>
-    //   <div>
-    //     <LoginForm />
-    //   </div>
-    // </div>
   );
 }
