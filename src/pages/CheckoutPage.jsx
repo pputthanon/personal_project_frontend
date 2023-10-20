908ae35bd52d0f28bccecd4a38e4937427f6b4ae
pputthanon <p.putthanon@gmail.com>
2023-10-20 21:15:49 +0700

    cart

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -7,13 +7,21 @@ export default function CheckoutPage() {
     <div>
       <div className="flex justify-center">
         <div className="w-2/3 ">
-          <div className="bg-white flex justify-center">Check out</div>
-          <div className="bg-blue-400">BaBaBa</div>
+          <div className="bg-white flex justify-center text-2xl mt-10 border rounded-t-xl py-2">
+            Check out
+          </div>
+          <div className="bg-blue-400 border rounded-b-xl">
+            <div>
+              <div>Total Price</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-center mt-10">
-        <div className="w-2/3 flex justify-end">
-          <button className="border p-2 bg-purple-300">Confirm Order </button>
+        <div className="w-2/3 flex justify-end ">
+          <button className="border p-2 bg-purple-300 rounded-xl">
+            Confirm Order
+          </button>
         </div>
       </div>
     </div>
